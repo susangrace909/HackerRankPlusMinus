708b4b80f20a380042a87a0c0ea779891ad3ff51
susangrace909 <102876452+susangrace909@users.noreply.github.com>
2022-10-19 17:06:33 -0400

    function written, small bug to fix line 43

--- plusMinus.js
+++ plusMinus.js
@@ -26,7 +26,7 @@ function readLine() {
  * The function accepts INTEGER_ARRAY arr as parameter.
  */
 
-/*Though process:
+/*Thought process:
 First need to identify # of integers in array
 Identify positive, negative, zero
 Convert to fraction
@@ -34,8 +34,30 @@ Fraction to decimal
 Display on separate lines
 */
 function plusMinus(arr) {
-  // Write your code here
-}
+  //variables
+  const positives = 0;
+  const negatives = 0;
+  const zeroes = 0;
+  //for loop
+  //counts number of positives, zeroes, negatives
+  for (int i=0; i<arr.size(); i++) {
+    if (arr.get(i) > 0) {
+      positives ++;
+    } else if (arr.get(i) == 0) {
+      zeroes++;
+    } else {
+        negatives++;
+      }
+    }
+//determines decimal
+positives /= arr.size();
+negatives /= arr.size();
+zeros /= arr.size();
+//prints numbers
+console.log(positives);
+console.log(negatives);
+console.log(zeroes);
+  };
 
 function main() {
   const n = parseInt(readLine().trim(), 10);
